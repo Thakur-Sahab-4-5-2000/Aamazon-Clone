9f8e7078d9b619f84c4eecf8a10a4e6062d00fdf
Shubham Kumar <56603941+Shubbbu@users.noreply.github.com>
2020-07-19 13:05:04 +0530

    Add files via upload

---
+++ build/precache-manifest.f0b1e4d375ebe11887bab7ae6a919e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f9981f4e991f9bb638edbd0a292230",
+    "url": "/index.html"
+  },
+  {
+    "revision": "305320df5dc0ef0069e7",
+    "url": "/static/css/main.1e3a17d4.chunk.css"
+  },
+  {
+    "revision": "8e7facd5d3871d88b923",
+    "url": "/static/js/2.a2f79037.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.a2f79037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305320df5dc0ef0069e7",
+    "url": "/static/js/main.1a637589.chunk.js"
+  },
+  {
+    "revision": "74ea84a75da3c5eb2b80",
+    "url": "/static/js/runtime-main.012d3530.js"
+  }
+]);
